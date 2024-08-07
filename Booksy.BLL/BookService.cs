a4f13a2bb23b20a823d91d472c851da62e1f7a20
matineno <martinsadogamhe9@gmail.com>
2024-08-06 20:33:07 -0500

    added classes to BookBLL

--- Booksy.BLL/BookService.cs
+++ Booksy.BLL/BookService.cs
@@ -3,11 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Booksy.Models;
+using Booksy.DAL;
 
 namespace Booksy.BLL
 {
     public class BookService
     {
         public class Placeholder { }
+        private readonly BookDAL _bookDAL;
+        public BookService(BookDAL bookDAL)
+        {
+            _bookDAL = bookDAL;
+        }
+        public List<Book> GetBooks()
+        {
+            return _bookDAL.GetBooks();
+        }
+        public IEnumerable<Book> GetAllBooks()
+        {
+            return _bookDAL.GetAllBooks();
+        }
+        public Book GetBook(int BookID)
+        {
+            return _bookDAL.GetBook(BookID);
+        }
+        public void AddBook(Book book)
+        {
+            _bookDAL.AddBook(book);
+        }
+        public void UpdateBook(Book book)
+        {
+            _bookDAL.UpdateBook(book);
+        }
+        public void DeleteBook(int BookID)
+        {
+            _bookDAL.DeleteBook(BookID);
+        }
     }
 }
