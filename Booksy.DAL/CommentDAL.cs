d6c041e1051a3e1843cf01d8a05dcfcaac1a815d
Yaroslav Fedorenko <y.fedorenko@gmail.com>
2024-08-07 09:58:49 -0500

    ID to Id changes

---
+++ Booksy.DAL/CommentDAL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Booksy.Models;
+
+namespace Booksy.DAL
+{
+    public class CommentDAL
+    {
+        public readonly BooksyDbContext _context;
+        public CommentDAL(BooksyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Comment>> GetCommentsAsync()
+        {
+            return await _context.Comment.ToListAsync();
+        }
+
+        public async Task<Product> GetProductAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task AddProductAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Booksy.Models/Comment.cs
+++ Booksy.Models/Comment.cs
@@ -9,13 +9,10 @@ namespace Booksy.Models
     public class Comment
     {
         public int CommentId { get; set; }
-
         public int BookId { get; set; }
         public string CommentText { get; set; }
         public string UserName { get; set; }
         public DateTime CommentTime { get; set; }
-
-
         public Book Book { get; set; }
     }
 }
