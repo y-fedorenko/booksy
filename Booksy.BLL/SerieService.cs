4ca0363631cb897222256198b8fb15b4c6c3af96
Yaroslav Fedorenko <y.fedorenko@gmail.com>
2024-08-07 13:56:40 -0500

    comment serie added

--- Booksy.BLL/SerieService.cs
+++ Booksy.BLL/SerieService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Booksy.DAL;
+using Booksy.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,36 @@ namespace Booksy.BLL
 {
     public class SerieService
     {
-        public class Placeholder { }
+        private readonly SerieDAL _dal;
+
+        public SerieService(SerieDAL dal)
+        {
+            _dal = dal;
+        }
+
+        public async Task<List<Serie>> GetSeriesAsync()
+        {
+            return await _dal.GetSeriesAsync();
+        }
+
+        public async Task<Serie> GetSerieAsync(int id)
+        {
+            return await _dal.GetSerieAsync(id);
+        }
+
+        public async Task AddSerieAsync(Serie serie)
+        {
+            await _dal.AddSerieAsync(serie);
+        }
+
+        public async Task UpdateSerieAsync(Serie serie)
+        {
+            await _dal.UpdateSerieAsync(serie);
+        }
+
+        public async Task DeleteSerieAsync(int id)
+        {
+            await _dal.DeleteSerieAsync(id);
+        }
     }
 }
