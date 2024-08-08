5f9653f6eb133aaeddef70d1836310d60836e07f
Yaroslav Fedorenko <y.fedorenko@gmail.com>
2024-08-08 01:26:04 -0500

    serie and comments sections

---
+++ Booksy.DAL/Migrations/20240808062532_UpdateComments.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booksy.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateComments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
